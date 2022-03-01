892443bcd08f5d244257a2b3d1e396145c4e9976
MarahDeven <marahdeven@gmail.com>
2022-03-01 09:21:09 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.linkedin.com/in/marah-deven-rice" target="_blank">
-          Marah Rice
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/MarahDeven/marahs-react-weather-app"
-          taget="-blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/marah-deven-rice"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Marah Rice
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/MarahDeven/marahs-react-weather-app"
+            taget="-blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
