fa571f2202cc2cb4bb0e56d3dc00b9652dbbf8cb
MarahDeven <marahdeven@gmail.com>
2022-03-01 09:07:09 -0500

    added github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.linkedin.com/in/marah-deven-rice" target="_blank">
+          Marah Rice
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/MarahDeven/marahs-react-weather-app"
+          taget="-blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
